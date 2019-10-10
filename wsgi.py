6c38bd0487db196aff395a539056714821962336
catherinelam <55800080+catherinelam@users.noreply.github.com>
2019-10-10 13:22:34 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -18,7 +18,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "I'm not hungry!\r\n", 200, { 'Content-Type': 'text/plain' }
+    return "I'm hungry!\r\n", 200, { 'Content-Type': 'text/plain' }
 
 if __name__ == '__main__':
     application.run(debug = True)
